052a02a6a9e4b345034f30182e14e27748978511
Matthew Mukasa <matthewmukasa0@gmail.com>
2026-02-03 15:31:00 +0300

    eda

--- src/app.js
+++ src/app.js
@@ -28,7 +28,11 @@ app.use(
   cors({
     origin: process.env.ALLOWED_ORIGINS
       ? process.env.ALLOWED_ORIGINS.split(',')
-      : ['http://localhost:3000', 'http://localhost:4000'],
+      : [
+          'http://localhost:3000',
+          'http://localhost:4000',
+          'https://marthms.netlify.app',
+        ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
